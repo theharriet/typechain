ddf9c7d938b7cfa7eb8142df1d16985c648863df
theharriet <goand123@gmail.com>
2021-10-22 16:09:05 +0900

    interface in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+    name: string,
+    age: number,
+    gender: string
 }
+
+const person = {
+    name: "harriet",
+    age: 24,
+    gender: "female"
+}
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+}
+//그럼 이제 person이 human interface랑 같은 구조인지 체크할거야 타입스크립트가 
+
+// const sayHi = (name: string, age: number, gender: string): string => {
+//     return `Hello ${name}, you are ${age}, you are a ${gender}`;
+// }
 
 //const sayHi = (name: string, age: number, gender: string): void => {
 //    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 //}
 
-sayHi("harriet", 24, "female");
+//sayHi("harriet", 24, "female");
+//console.log(sayHi(person)); 이렇게 하면 안돼 sayHi()는 3개의 argument를 예상하고 있기에
+// object로 전달하면 가능 --> interface해보자
+
+//이런식으로 interface를 사용해서 object를 사용하면
+// 블록체인의 경우 하나의 블록을 interface로 할 수 있음
+
+
+
 
 export {};
-
-//sayHi(name, age); 자바스크립트에서 이렇게 돌리면 gender가 undefined뜨는데 타입스크립트에서는
-// 미리 알려줌. 컴파일도 안됨
-
-//const sayHi = (name, age, gender?) ==> 이렇게 하면 gender는 넣어도 되고 안넣어도 되는 매개변수
